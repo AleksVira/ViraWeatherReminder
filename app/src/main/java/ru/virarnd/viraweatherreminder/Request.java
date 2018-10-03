1187506428468658302604a91dbdaa6c0f94f74b
Aleksey <123all@newmail.ru>
2018-10-03 23:07:27 +0300

    Homework 3, две активности и передача данных между ними

--- app/src/main/java/ru/virarnd/viraweatherreminder/Request.java
+++ app/src/main/java/ru/virarnd/viraweatherreminder/Request.java
@@ -9,5 +9,6 @@ public class Request extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+        // Здесь будет добавлен код
     }
 }
